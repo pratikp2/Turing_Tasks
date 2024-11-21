d3d74f8339895a6c73e8b0555f95a71479bcf640
Pratik Patil <pppratikpatil723@gmail.com>
2024-11-22 02:11:30 +0530

    Task added

---
+++ Task-Cmake/build/CMakeFiles/Example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Example.dir/link.d"
+  "CMakeFiles/Example.dir/main.cpp.o"
+  "CMakeFiles/Example.dir/main.cpp.o.d"
+  "Example"
+  "Example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Task-Cmake/build/CMakeFiles/myapp.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myapp.dir/link.d"
+  "CMakeFiles/myapp.dir/main.cpp.o"
+  "CMakeFiles/myapp.dir/main.cpp.o.d"
+  "CMakeFiles/myapp.dir/src/file1.cpp.o"
+  "CMakeFiles/myapp.dir/src/file1.cpp.o.d"
+  "CMakeFiles/myapp.dir/src/file2.cpp.o"
+  "CMakeFiles/myapp.dir/src/file2.cpp.o.d"
+  "myapp"
+  "myapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
