4d6503413f12dcc5baa5aea0c1b22d9557a276ef
Pratik Patil <pppratikpatil723@gmail.com>
2025-01-03 02:06:04 +0530

    Added new .cpp files for Task-499097

--- Task-Cmake/my_project/build/CMakeFiles/superbuild.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/superbuild"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/superbuild.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Task-Cmake/my_project/build/module2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Code_Setup/Turing/Turing_Tasks/Task-Cmake/my_project/module2
-# Build directory: /mnt/c/Code_Setup/Turing/Turing_Tasks/Task-Cmake/my_project/build/module2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
